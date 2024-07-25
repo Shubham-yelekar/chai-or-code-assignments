4f2df1ccd0bc1600af90672831295e457f0459ae
shubham <shubhamyelekar0@gmail.com>
2024-07-25 15:30:37 +0530

    initialize

---
+++ Masterji/Machine-Coding-Round-React/OTP-form/src/App.jsx
@@ -0,0 +1,28 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className='content-wrapper'>
+      <h1>
+        Otp Form
+      </h1>
+      <div className='otp-wrapper'>
+        <h3>Mobile Phone Verification</h3>
+        <p>Enter the 4-digit verification code that was sent to <br /> your phone number.</p>
+        <form action=""></form>
+        <p>Didn’t receive code? Resend </p>
+
+      </div>
+      <a href="" className='chai-code-logo'>
+
+      </a>
+
+    </div>
+
+  )
+}
+
+export default App
